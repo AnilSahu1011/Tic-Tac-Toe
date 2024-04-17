ff21f5a483a0dcb28faf885a5f3cef64ec64038c
Anila Sahu <99677483+AnilSahu1011@users.noreply.github.com>
2024-04-17 20:26:52 +0530

    Object initialized

--- Game.java
+++ Game.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 class TicTacToe{
     static char[][] board;
 
-    public TicTacToe()
-    {
+    public TicTacToe() {
         board = new char[3][3];
         initializeBoard();
     }
     void initializeBoard(){
         for(int i=0; i<board.length; i++){
-            for(int j=0; j<board[0].length; j++){
+            for(int j=0; j<board[i].length; j++){
                 board[i][j] = ' ';
             }
         }
@@ -20,13 +19,14 @@ class TicTacToe{
         System.out.println("-------------");
         for(int i=0; i<board.length; i++){
             System.out.print("| ");
-            for(int j=0; j<board[0].length; j++){
+            for(int j=0; j<board[i].length; j++){
                 System.out.print(board[i][j] + " | ");
             }
             System.out.println();
             System.out.println("-------------");
         }
     }
+
     static void placeMark(int row, int col, char symbol){
         if( row>=0 && row<=2 && col>=0 && col<=2 ){
             board[row][col] = symbol;
@@ -35,6 +35,7 @@ class TicTacToe{
             System.out.println("Invalid input");
         }
     }
+
     static boolean checkColumnWin(){
         for (int j=0;j<=2;j++){
             if(board[0][j] != ' ' && board[0][j] == board[1][j] && board[1][j] == board[2][j]){
@@ -43,6 +44,7 @@ class TicTacToe{
         }
         return false;
     }
+
     static boolean checkRowWin(){
         for (int i=0;i<=2;i++){
             if(board[i][0] != ' ' && board[i][0] == board[i][1] && board[i][1] == board[i][2]){
@@ -51,11 +53,10 @@ class TicTacToe{
         }
         return false;
     }
+
     static boolean checkDiagonalWin(){
-        if(board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) {
-            return true;
-        }
-        if(board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0]){
+        if(board[0][0] != ' ' && board[0][0] == board[1][1] && board[1][1] == board[2][2]
+                || (board[0][2] != ' ' && board[0][2] == board[1][1] && board[1][1] == board[2][0]) ) {
             return true;
         }
         return false;
@@ -71,16 +72,14 @@ class TicTacToe{
         }
         return true;
     }
-
 }
 
-abstract class Player{
-    String name;
-    char mark;
-    abstract boolean isValidMove(int row, int col);
+interface Player{
+    void makeMove();
+    boolean isValidMove(int row, int col);
 }
-class HumanPlayer extends Player{
-    Scanner sc = new Scanner(System.in);
+class HumanPlayer implements Player{
+
     String name;
     char mark;
 
@@ -88,17 +87,20 @@ class HumanPlayer extends Player{
         this.name = name;
         this.mark = mark;
     }
-    void makeMove(){
-        int row,col;
-       do{
-           System.out.println("Enter the Row and Column: ");
-           row = sc.nextInt();
-           col = sc.nextInt();
-       }while (!isValidMove(row,col));
 
-       TicTacToe.placeMark(row,col,mark);
+    public void makeMove(){
+        Scanner sc = new Scanner(System.in);
+        int row,col;
+        System.out.println("Enter the Row and Column: ");
+        do
+        {
+            row = sc.nextInt();
+            col = sc.nextInt();
+        }while (!isValidMove(row,col));
+        TicTacToe.placeMark(row,col,mark);
     }
-    boolean isValidMove(int row, int col){
+
+    public boolean isValidMove(int row, int col){
         if(row>=0 && row<=2 && col>=0 && col<=2){
             if(TicTacToe.board[row][col] == ' '){
                 return true;
@@ -110,6 +112,7 @@ class HumanPlayer extends Player{
 
 public class Game {
     public static void main(String[] args) {
+        TicTacToe t = new TicTacToe();
         HumanPlayer p1 = new HumanPlayer("Anil",'X');
         HumanPlayer p2 = new HumanPlayer("Mama",'O');
 
@@ -123,10 +126,12 @@ public class Game {
             if(TicTacToe.checkRowWin() || TicTacToe.checkColumnWin() || TicTacToe.checkDiagonalWin()) {
                 System.out.println(currentPlayer.name + " has Won ! ! !");
                 break;
-            } else if (TicTacToe.isFull()) {
+            }
+            else if (TicTacToe.isFull()) {
                 System.out.println("Nobody Won\nEnd...");
                 break;
-            } else{
+            }
+            else{
                 if(currentPlayer==p1){
                     currentPlayer = p2;
                 }
